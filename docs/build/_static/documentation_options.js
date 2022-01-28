06eb0dd8f00cdfcd42a3b9630dcef0ed5ab03a16
dylanmcreynolds <dylanmcreynolds@users.noreply.github.com>
2022-01-28 16:22:10 +0000

    deploy: 79809ad9c0d8d3172f8a6fc06608f5b806337ac2

--- docs/build/_static/documentation_options.js
+++ docs/build/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post44+g6938eb3',
+    VERSION: '0.post101+g79809ad',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
